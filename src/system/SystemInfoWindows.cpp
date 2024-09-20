6c2b4d570bbf5a0a72d4be625731f715d0cacd05
Meldryt <herrdersuppen@gmail.com>
2024-09-20 19:57:21 +0200

    Add windows os name and version.

--- src/system/SystemInfoWindows.cpp
+++ src/system/SystemInfoWindows.cpp
@@ -1,12 +1,40 @@
 #include "SystemInfoWindows.h"
 
 //#include "../Utils.h"
-
+#ifdef _WIN32
+#include <windows.h>
+#endif
 #include <QDebug>
+
+#ifdef _WIN32
+typedef LONG NTSTATUS, * PNTSTATUS;
+#define STATUS_SUCCESS (0x00000000)
+
+typedef NTSTATUS(WINAPI* RtlGetVersionPtr)(PRTL_OSVERSIONINFOW);
+
+RTL_OSVERSIONINFOW GetRealOSVersion() {
+    HMODULE hMod = ::GetModuleHandleW(L"ntdll.dll");
+    if (hMod) {
+        RtlGetVersionPtr fxPtr = (RtlGetVersionPtr)::GetProcAddress(hMod, "RtlGetVersion");
+        if (fxPtr != nullptr) {
+            RTL_OSVERSIONINFOW rovi = { 0 };
+            rovi.dwOSVersionInfoSize = sizeof(rovi);
+            if (STATUS_SUCCESS == fxPtr(&rovi)) {
+                return rovi;
+            }
+        }
+    }
+    RTL_OSVERSIONINFOW rovi = { 0 };
+    return rovi;
+}
+
+#endif
 
 SystemInfoWindows::SystemInfoWindows()
 {
     qDebug() << __FUNCTION__;
+
+    m_osName = "Windows";
 }
 
 SystemInfoWindows::~SystemInfoWindows()
@@ -22,28 +50,16 @@ bool SystemInfoWindows::init()
 
 void SystemInfoWindows::readStaticInfo()
 {
-    //const std::string strPrettyName = "PRETTY_NAME=";
-    //m_osName = Utils::exec("cat /etc/os-release | grep " + strPrettyName);
-    //if(!m_osName.empty())
-    //{
-    //    m_osName.erase(std::remove(m_osName.begin(), m_osName.end(), '"'), m_osName.end());
-    //    m_osName.erase(m_osName.find(strPrettyName), strPrettyName.length());
-    //}
-//
-    //const std::string strVersion = "VERSION=";
-    //m_osVersion = Utils::exec("cat /etc/os-release | grep " + strVersion);
-    //if(!m_osVersion.empty())
-    //{
-    //    m_osVersion.erase(std::remove(m_osVersion.begin(), m_osVersion.end(), '"'), m_osVersion.end());
-    //    m_osVersion.erase(m_osVersion.find(strVersion), strVersion.length());
-    //    m_osCodeName = m_osVersion;
-    //    const size_t pos = m_osCodeName.find_first_of("(");
-    //    m_osCodeName = m_osCodeName.substr(pos+1, (m_osCodeName.find_last_of(")") - pos)-1);
-    //    m_osVersion = m_osVersion.substr(0, pos-1);
-    //}
-//
-    //qDebug() << __FUNCTION__ << " m_osName:" << m_osName << " m_osVersion:" << m_osVersion << " m_osCodeName:" << m_osCodeName;
-}//
+#ifdef _WIN32
+    RTL_OSVERSIONINFOW osInfo = GetRealOSVersion();
+
+    const std::string strVersion = std::to_string(osInfo.dwMajorVersion) + "." + std::to_string(osInfo.dwMinorVersion);
+    const std::string strBuildNumber = std::to_string(osInfo.dwBuildNumber);
+
+    m_osName = "Windows " + strVersion;
+    m_osVersion = strBuildNumber;
+#endif
+}
 
 void SystemInfoWindows::readDynamicInfo()
 {
